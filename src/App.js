107cfd3c0006df476b107dc682683304ae9b306f
Diana-Cano <dianacanoo@icloud.com>
2023-04-20 18:20:46 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 
 function App() {
-  return (
+const [gitHubName, setGitHubName] = useState('')
+const [gitHubUrl, setGitHubUrl] = useState('')
+
+
+useEffect(() => {
+    fetch('https://api.github.com/users/Olafaloofian')
+    .then(res => res.json())
+    .then(data => {
+        setGitHubName(data.name)
+        setGitHubUrl(data.html_url)
+    })
+}, [])
+
+return (
     <div className="App">
-      <h1>Github Profile Info:</h1>
+        <h1>Github Profile Info:</h1>
+        <h2>{gitHubName}</h2>
+        <a href={gitHubUrl}>Go to profile</a>
     </div>
-  );
+);
 }
 
-export default App;
+export default App
